46c634e2f8e1e14fefbd0567b0f2694f0f058243
AlenaBu <abubenshchykava@gmail.com>
2021-12-15 15:56:01 -0500

    Second

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-	for (int i=0; i<5; i++){
+	for (int i=0; i<8; i++){
         System.out.println("batch 10 is great");
         Thread.sleep(2000);
 
